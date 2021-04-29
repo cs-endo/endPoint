449369b5681f110c75cbf36347e71aafef3700b7
CoSa <co.sa@tuta.io>
2021-04-29 11:04:31 +0100

    Add initial diffs and clean up download.sh

--- Video/download.sh
+++ Video/download.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+function youtube {
+	method='youtube-dl -i \
+-o "%(title)s" '
+
+	[[ -n $1 ]] && $method $@ || echo "$method"
+}
+
 function youtube480 {
-	method='youtube-dl \
--o "%(title)s" -i \
+	method='youtube-dl -i \
+-o "%(title)s" \
 -f "bestvideo[height<=480]+bestaudio/best[height<=480]" '
 
         [[ -n $1 ]] && $method $@ || echo "$method"
 }
 
 function youtube480GT {
-	method='youtube-dl \
--o "%(playlist_index)s-%(title)s.%(ext)s" -i \
+	method='youtube-dl -i \
+-o "%(playlist_index)s-%(title)s.%(ext)s" \
 -f "bestvideo[height<=480]+bestaudio/best[height<=480]" '
 
 	[[ -n $1 ]] && $method $@ || echo "$method"
