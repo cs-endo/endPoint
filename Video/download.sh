017988d15768f99fe7e55018a1dae297d7186beb
CoSa <co.sa@tuta.io>
2021-05-07 22:49:08 +0100

    Improve video scripts to format properly

--- Video/download.sh
+++ Video/download.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
 function youtube {
-	method='youtube-dl -i \
--o "%(title)s" '
+	method="youtube-dl -i \
+-o %(title)s "
 
 	[[ -n $1 ]] && $method $@ || echo "$method"
 }
 
+function youtube720 {
+        method="youtube-dl -i \
+-o %(title)s \
+-f best[height<=?720]+bestaudio/best "
+
+        [[ -n $1 ]] && $method $@ || echo "$method"
+}
+
 function youtube480 {
-	method='youtube-dl -i \
--o "%(title)s" \
--f "bestvideo[height<=480]+bestaudio/best[height<=480]" '
+	method="youtube-dl -i \
+-o %(title)s \
+-f best[height<=?480]+bestaudio/best "
 
         [[ -n $1 ]] && $method $@ || echo "$method"
 }
 
 function youtube480GT {
-	method='youtube-dl -i \
--o "%(playlist_index)s-%(title)s.%(ext)s" \
--f "bestvideo[height<=480]+bestaudio/best[height<=480]" '
+	method="youtube-dl -i \
+-o %(playlist_index)s-%(title)s.%(ext)s \
+-f best[height<=?480]+bestaudio/best "
 
 	[[ -n $1 ]] && $method $@ || echo "$method"
 }
