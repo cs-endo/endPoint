e348b846f8c6a2632ece540b0f8923e50e24f91f
CoSa <co.sa@tuta.io>
2021-07-20 01:15:46 +0100

    Clean up variables and add comments

--- Video/ytdl.sh
+++ Video/ytdl.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
 function ytdlConf {
-	argOut="$1"	# a V v v360 v480 v720...
-	argFile="$2"	# p=index, t=title, u=date, x=ext
-			# $@ = additional parameters + URL
-	shift 2
+	argAV="$1"	# $1: [a/v/V][###] - e.g. v480 for 480p video
+	argFname="$2"	# $2: p=index, t=title, u=date, x=ext
+	shift 2		# $@: additional parameters + URL
 
-	declare -A outputs=( \
+	declare -A dictOutFormat=( \
 ["v"]="--merge-output-format mp4" \
 ["V"]="--merge-output-format mp4 --postprocessor-args '-s hd360 -crf 24 -strict -2 -c:v libx264 -c:a aac'" \
 ["a"]="-x --audio-format mp3" )
 
-	declare -A name_forms=( \
+	declare -A dictFileName=( \
 ["tx"]="-o '%(title)s.%(ext)s'" \
 ["ptx"]="-o '%(playlist_index)3d-%(title)s.%(ext)s'" \
 ["utx"]="-o '%(upload_date)s_%(title)s.%(ext)s'" )
 
 
-	outType="${argOut::1}"
-	outRes="${argOut:1}"
-	[[ -z $outRes ]] || outRes="-f best[height=$outRes]+bestaudio/best"
+	# If resolution is given, specify the download to be of that resolution
+	avType="${argAV::1}"	# e.g. v480 --> v
+	avRes="${argAV:1}"	# e.g. v480 --> 480
+	[[ -z $avRes ]] || avInpFormat="-f best[height=$avRes]+bestaudio/best"
 
-	[[ -z $1 ]] && base="echo youtube-dl -i" || base="youtube-dl -i"
-	$base ${outputs[$outType]} ${name_forms[$argFile]} $outRes $@
+	# If no URL given, print the command otherwise executed
+	baseCom="youtube-dl -i"
+	[[ -z $1 ]] && baseCom="echo $baseCom"
+	$baseCom $avInpFormat ${dictOutFormat[$avType]} ${dictFileName[$argFName]} $@
 }
 
-function youtube { 	ytdlConf v tx $@; }
-function youtube480 {	ytdlConf v480 tx $@; }
-function youtube720 {	ytdlConf v720 tx $@; }
-function youtube480GT {	ytdlConf v720 ptx $@; }
-function youtubeA {	ytdlConf a tx $@; }
+function youtube	{ ytdlConf v tx $@; }
+function youtube480	{ ytdlConf v480 tx $@; }
+function youtube480GT   { ytdlConf v720 ptx $@; }
+function youtube720	{ ytdlConf v720 tx $@; }
+function youtubeA	{ ytdlConf a tx $@; }
 
-function twitch { 	ytdlConf v360 utx $@ || ytdlConf V utx $@; }
+function twitch		{ ytdlConf v360 utx $@ || ytdlConf V utx $@; }
